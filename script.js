8bf0574ba00ee7831210833ff9de7f7410b0535e
Potomac Glen <glenpotomac@gmail.com>
2025-07-16 19:41:06 -0400

    add timeline

---
+++ script.js
@@ -0,0 +1,13 @@
+fetch('posts.json')
+  .then(res => res.json())
+  .then(data => {
+    const feed = document.getElementById('blog-feed');
+    if (!feed) return;
+    feed.innerHTML = '';
+    data.forEach(post => {
+      const div = document.createElement('div');
+      div.className = 'post';
+      div.innerHTML = `<h3>${post.title}</h3><p><small>${new Date(post.date).toLocaleDateString()}</small></p><p>${post.content}</p>`;
+      feed.appendChild(div);
+    });
+  });
